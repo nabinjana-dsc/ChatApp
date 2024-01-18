3ed02d1cdb2d880e609c8969e1e4e0b78b7787af
nabinjana255 <nabinjana7@gmail.com>
2024-01-18 10:46:45 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+
+const Config = {
+  apiKey: 'AIzaSyBz58TRXbfKBkpuQyYv5QBTx6BgK3ysl_Y',
+  authDomain: 'chat-app-73bff.firebaseapp.com',
+  databaseURL: 'https://chat-app-73bff-default-rtdb.firebaseio.com',
+  projectId: 'chat-app-73bff',
+  storageBucket: 'chat-app-73bff.appspot.com',
+  messagingSenderId: '397070434917',
+  appId: '1:397070434917:web:341caa413588b6cc097c8c',
+  measurementId: 'G-46KH7JW57V',
+};
+
+
+const app = firebase.initializeApp(Config);
