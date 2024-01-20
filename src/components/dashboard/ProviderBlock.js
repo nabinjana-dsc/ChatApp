f6df06e0baa8e63134568fc7d7c18eebe9ffc876
nabinjana255 <nabinjana7@gmail.com>
2024-01-20 12:24:19 +0530

    linked github and google to one account, created provider block

---
+++ src/components/dashboard/ProviderBlock.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { Alert, Button, Icon, Tag } from 'rsuite';
+import firebase from 'firebase/app';
+import { auth } from '../../misc/firebase';
+
+const ProviderBlock = () => {
+  const [isConnected, setIsConnected] = useState({
+    'google.com': auth.currentUser.providerData.some(
+      data => data.providerId === 'google.com'
+    ),
+    'github.com': auth.currentUser.providerData.some(
+      data => data.providerId === 'github.com'
+    ),
+  });
+
+  const updateIsConnected = (providerId, value) => {
+    setIsConnected(p => {
+      return {
+        ...p,
+        [providerId]: value,
+      };
+    });
+  };
+
+  const unlink = async providerId => {
+    try {
+      if (auth.currentUser.providerData.length === 1) {
+        throw new Error(`You can not disconnect from ${providerId}`);
+      }
+
+      await auth.currentUser.unlink(providerId);
+      updateIsConnected(providerId, false);
+      Alert.info(`Disconnected from ${providerId}`, 4000);
+    } catch (error) {
+      Alert.error(error.message, 4000);
+    }
+  };
+
+  const unlinkGithub = () => {
+    unlink('github.com');
+  };
+  const unlinkGoogle = () => {
+    unlink('google.com');
+  };
+
+  const link = async provider => {
+    try {
+      await auth.currentUser.linkWithPopup(provider);
+      Alert.info(`Linked to ${provider.providerId}`, 4000);
+
+      updateIsConnected(provider.providerId, true);
+    } catch (error) {
+      Alert.error(error.message, 4000);
+    }
+  };
+
+  const linkGithub = () => {
+    link(new firebase.auth.GithubAuthProvider());
+  };
+  const linkGoogle = () => {
+    link(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <div>
+      {isConnected['google.com'] && (
+        <Tag color="green" closable onClose={unlinkGoogle}>
+          <Icon icon="google" /> Connected
+        </Tag>
+      )}
+      {isConnected['github.com'] && (
+        <Tag color="black" closable onClose={unlinkGithub}>
+          <Icon icon="github" /> Connected
+        </Tag>
+      )}
+
+      <div className="mt-2">
+        {!isConnected['google.com'] && (
+          <Button block color="green" onClick={linkGoogle}>
+            <Icon icon="google" /> Link to Google
+          </Button>
+        )}
+
+        {!isConnected['github.com'] && (
+          <Button block color="black" onClick={linkGithub}>
+            <Icon icon="github" /> Link to Github
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProviderBlock;
