bc1d8ea1130b907c8f5018a7eef17b682a77b68e
nabinjana255 <nabinjana7@gmail.com>
2024-01-20 08:58:26 +0530

    added useMediaQuery hook and made drawer responsive

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,17 +1,27 @@
-import { Button, Drawer, Icon } from 'rsuite';
-import { useModalState } from '../../misc/custom-hooks';
+import { Alert, Button, Drawer, Icon } from 'rsuite';
+import { useMediaQuery, useModelState } from '../../misc/custom-hooks';
 import Dashboard from '.';
+import { useCallback } from 'react';
+import { auth } from '../../misc/firebase';
 
 const DashboardToggle = () => {
-  const { isOpen, close, open } = useModalState();
+  const { isOpen, close, open } = useModelState();
+  const isMobile = useMediaQuery('(max-width: 422px)');
+
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+    Alert.info('Signed out', 4000);
+    close( );
+  }, [close]);
 
   return (
     <>
       <Button block color="blue" onClick={open}>
-        <Icon icon="dashboard" /> Dashboard
+        <Icon icon="dashboard" />
+        Dashboard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
+        <Dashboard  onSignOut = {onSignOut} />
       </Drawer>
     </>
   );
